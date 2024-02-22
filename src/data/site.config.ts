812ddb7b412135564a838aac84f5ca87e965db9c
Jesus Gutierrez <jesusg.1301.jg@gmail.com>
2024-02-22 09:21:21 -0400

    chores: starter config files for the project

---
+++ src/data/site.config.ts
@@ -0,0 +1,19 @@
+interface SiteConfig {
+    author: string;
+    title: string;
+    description: string;
+    lang: string;
+    ogLocale: string;
+    shareMessage: string;
+    paginationSize: number;
+}
+
+export const siteConfig: SiteConfig = {
+    author: 'yidev27', // Site author
+    title: 'Gptscribe', // Site title.
+    description: 'Count on our handy pros to nail those home fixes with expertise and a guarantee – we get it right the first time, every time!', // Description to display in the meta tags
+    lang: 'en-GB',
+    ogLocale: 'en_GB',
+    shareMessage: 'Share this post', // Message to share a post on social media
+    paginationSize: 6, // Number of posts per page
+};
